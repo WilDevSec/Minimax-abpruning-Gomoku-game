3baf664821bc769dfb295599b9b68f2edfaadf92
wilhutchens <33008564+wilhutchens@users.noreply.github.com>
2021-03-02 14:35:10 +0000

    Update player.py

--- player.py
+++ player.py
@@ -15,40 +15,40 @@ from gomokuAgent import GomokuAgent
 class Player(GomokuAgent):
 
     # Returns the first position that will create X number of pieces in a line horizontally
-    # def nInARowMove(playerID, board, X_IN_A_LINE):
-    #     for i in range(0, 11):
-    #         count = 0
-    #         for j in range(0, 11):
-    #             if count >= X_IN_A_LINE - 1:
-    #                 if board[i, j] == 0:
-    #                     return i, j
-    #                 elif board[i, j - 6] == 0:
-    #                     return i, j - 6
-    #             if board[i, j] != playerID:
-    #                 count = 0
-    #             else:
-    #                 count += 1
-    #     return None, None
+    def nInARowMove(self, playerID, board, X_IN_A_LINE):
+        for i in range(0, 11):
+            count = 0
+            for j in range(0, 11):
+                if count >= X_IN_A_LINE - 1:
+                    if board[i, j] == 0:
+                        return i, j
+                    elif board[i, j - 6] == 0:
+                        return i, j - 6
+                if board[i, j] != playerID:
+                    count = 0
+                else:
+                    count += 1
+        return None, None
 
-    # Returns same as above but diagonally
-    # def nInDiagMove(playerID, board, X_IN_A_LINE):
-    #     BOARD_SIZE = board.shape[0]
-    #     for r in range(BOARD_SIZE - X_IN_A_LINE + 1):
-    #         for c in range(BOARD_SIZE - X_IN_A_LINE + 1):
-    #             count = 0
-    #             for i in range(X_IN_A_LINE):
-    #                 if count >= X_IN_A_LINE - 1:
-    #                     if board[r + i + 1, c + i + 1] == 0:
-    #                         return r + i + 1, c + i + 1
-    #                     elif board[r - 1, c - 1] == 0:
-    #                         return r - 1, c - 1
-    #                 if board[r + i, c + i] != playerID:
-    #                     count = 0
-    #                 else:
-    #                     count += 1
-    #     return None, None
+    # Returns the first position that will create X number of pieces in a line diagonally
+    def nInDiagMove(self, playerID, board, X_IN_A_LINE):
+        BOARD_SIZE = board.shape[0]
+        for r in range(BOARD_SIZE - X_IN_A_LINE + 1):
+            for c in range(BOARD_SIZE - X_IN_A_LINE + 1):
+                count = 0
+                for i in range(X_IN_A_LINE):
+                    if count >= X_IN_A_LINE - 1:
+                        if r + i != 10 and c + i != 10 and board[r + i + 1, c + i + 1] == 0:
+                            return r + i + 1, c + i + 1
+                        elif board[r - 1, c - 1] == 0:
+                            return r - 1, c - 1
+                    if board[r + i, c + i] != playerID:
+                        count = 0
+                    else:
+                        count += 1
+        return None, None
 
-    # Returns the number given player's pieces in an unbroken row of a given length X
+    # Returns the number of times unbroken rows of length X of player's pieces are on the board
     def nInARowCount(self, playerID, board, X_IN_A_LINE):
         count = 0
         for i in range(self.BOARD_SIZE):
@@ -66,7 +66,7 @@ class Player(GomokuAgent):
                     consecutiveCount += 1
         return count
 
-    # Returns number of given player's pieces in unbroken diagonal line of length X
+    # Returns the number of times unbroken diagonal lines of length X of player's pieces are on the board
     def diagCount(self, playerID, board, X_IN_A_LINE):
         count = 0
         for r in range(self.BOARD_SIZE - X_IN_A_LINE + 1):
@@ -105,23 +105,29 @@ class Player(GomokuAgent):
         score += self.diagCount(playerID, board, 2)
         return score
 
-    # Minimax algorithm, returns an evaluation of the board if the max depth has been reached
-    # Otherwise it calls itself recursively with the children of the node and depth -1
-    # AB pruning checks that
+    """ Minimax algorithm with AB pruning, returns an evaluation of the board if the max depth has been reached Otherwise it calls
+    itself recursively with the children of the node and depth -1. Alpha and Beta represent the minimum score that
+    the maximizing player is assured of and the maximum score that the minimizing player is assured of."""
+
     def minimax(self, playerID, board, depth, alpha, beta, maximizingPlayer):
         # Checking for a winning move
-        # i, j = nInARowMove(maximizingPlayer, board, 5)
-        # x, y = nInDiagMove(maximizingPlayer, board, 5)
+        i, j = self.nInARowMove(maximizingPlayer, board, 5)
+        x, y = self.nInDiagMove(maximizingPlayer, board, 5)
         if depth == 0:
-            return [self.evaluateScore(playerID, board), None, None]
-        # if x is not None:
-        #     return [evaluateScore(maximizingPlayer, board), x, y]
+            return [1000, None, None]
+        if i is not None:
+            return [1000, i, j]
+        if x is not None:
+            return [self.evaluateScore(maximizingPlayer, board), x, y]
         if maximizingPlayer:
             maxEval = [-1_000_000, None, None]
             for child in self.children(playerID, board):
                 evaluation = self.minimax(1 if playerID == -1 else -1, child[0], depth - 1, alpha, beta, False)
                 maxEval = maxEval if maxEval[0] > evaluation[0] else [evaluation[0], child[1], child[2]]
                 alpha = max(alpha, evaluation[0])
+                """" When beta <= alpha, an further value larger than beta will not chosen by minimising player,
+                and any value smaller than beta will not be chosen by maximising player. So further
+                nodes do not need to be searched"""
                 if beta <= alpha:
                     break
             return maxEval
@@ -135,41 +141,7 @@ class Player(GomokuAgent):
                     break
             return minEval
 
-    # def minimax(board, depth, alpha, beta, maximizingPlayer):
-    #     # Setting opposing player's ID
-    #     if maximizingPlayer == 1:
-    #         minimizingPlayer = -1
-    #     else:
-    #         minimizingPlayer = 1
-    #     # Checking for a winning move
-    #     # i, j = nInARowMove(maximizingPlayer, board, 5)
-    #     # x, y = nInDiagMove(maximizingPlayer, board, 5)
-    #     if depth == 0:
-    #         return [evaluateScore(maximizingPlayer if maximizingPlayer else minimizingPlayer, board), None, None]
-    #     # if x is not None:
-    #     #     return [evaluateScore(maximizingPlayer, board), x, y]
-    #     if maximizingPlayer:
-    #         maxEval = [-1_000_000, None, None]
-    #         for child in children(maximizingPlayer, board):
-    #             evaluation = minimax(child[0], depth - 1, alpha, beta, False)
-    #             maxEval = maxEval if maxEval[0] > evaluation[0] else [evaluation[0], child[1], child[2]]
-    #             previousMoves.append(maxEval)
-    #             alpha = max(alpha, evaluation[0])
-    #             if beta <= alpha:
-    #                 break
-    #         return maxEval
-    #     else:
-    #         minEval = [1_000_000, None, None]
-    #         for child in children(minimizingPlayer, board):
-    #             evaluation = minimax(child[0], depth - 1, alpha, beta, True)
-    #             minEval = minEval if minEval[0] < evaluation[0] else [evaluation[0], child[1], child[2]]
-    #             previousMoves.append(minEval)
-    #             beta = min(beta, evaluation[0])
-    #             if beta <= alpha:
-    #                 break
-    #         return minEval
-
-    # Return list of board states and their moves after all viable moves have taken place
+    # Returns list of board states and their moves after all viable moves have taken place
     def children(self, playerID, board):
         BOARD_SIZE = board.shape[0]
         boardList = []
@@ -216,9 +188,11 @@ class Player(GomokuAgent):
             return 5, 5
         evaluations = []
         moves = []
+        # Performs minimax and all possible moves from given board state
         for child in self.children(self.ID, board):
             moves.append([child[1], child[2]])
             evaluation = self.minimax(self.ID, child[0], 2, -1_000_000, 1_000_000, True)
             evaluations.append(evaluation[0])
             bestValueIndex = np.argmax(np.array(evaluations))
+        # Returns move that ends with the highest heuristic score (evaluation)
         return moves[bestValueIndex][0], moves[bestValueIndex][1]
